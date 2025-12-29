81e7b79cb81f7af93f6f005f2dc7ff038601f448
mazizmani <70183467+mazizmani@users.noreply.github.com>
2025-12-29 03:26:54 +0100

    Add files via upload

---
+++ Day1/build-a-sentence-maker.js
@@ -0,0 +1,37 @@
+** start of script.js **
+
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+// Going to be honest, I just used ChatGPT to fill the variables in for me.
+// so I can focus on actually putting the code together
+adjective = "mysterious";
+noun = "robot";
+verb = "sleeping";
+place = "abandoned warehouse";
+adjective2 = "weird";
+noun2 = "control panels";
+
+let firstStory;
+
+firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "clumsy";
+noun = "wizard";
+verb = "running";
+place = "enchanted forest";
+adjective2 = "sparkling";
+noun2 = "spellbooks";
+
+let secondStory;
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
+//This is from freeCodeCamp, and i was stunned for about a minute or so why isn't it accepting my code
+//Turns out i did have to put in the requested (n) after the a...
+** end of script.js **
+
